08220e3789f5308bd397898f089f996e0d66be09
Marco Nicola <marconicola@disroot.org>
2021-09-17 20:49:49 +0200

    Remove deprecated CancelRequest

--- transport.go
+++ transport.go
@@ -6,9 +6,7 @@ package oauth2
 
 import (
 	"errors"
-	"log"
 	"net/http"
-	"sync"
 )
 
 // Transport is an http.RoundTripper that makes OAuth 2.0 HTTP requests,
@@ -53,18 +51,6 @@ func (t *Transport) RoundTrip(req *http.Request) (*http.Response, error) {
 	// req.Body is assumed to be closed by the base RoundTripper.
 	reqBodyClosed = true
 	return t.base().RoundTrip(req2)
-}
-
-var cancelOnce sync.Once
-
-// CancelRequest does nothing. It used to be a legacy cancellation mechanism
-// but now only it only logs on first use to warn that it's deprecated.
-//
-// Deprecated: use contexts for cancellation instead.
-func (t *Transport) CancelRequest(req *http.Request) {
-	cancelOnce.Do(func() {
-		log.Printf("deprecated: github.com/stuffingo/oauth2: Transport.CancelRequest no longer does anything; use contexts")
-	})
 }
 
 func (t *Transport) base() http.RoundTripper {
